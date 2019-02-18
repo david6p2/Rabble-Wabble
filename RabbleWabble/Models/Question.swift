4ffcf84d5b52de9f80fbf2324d91c92e62a1ca4a
David Andres Cespedes <david.x.cespedes.-nd@disney.com>
2019-02-18 10:44:24 -0500

    Applying MVC

---
+++ RabbleWabble/Models/Question.swift
@@ -0,0 +1,15 @@
+//
+//  Question.swift
+//  RabbleWabble
+//
+//  Created by David Andres Cespedes on 6/5/18.
+//  Copyright © 2018 David Andres Cespedes. All rights reserved.
+//
+
+import Foundation
+
+public struct Question {
+    public let answer: String
+    public let hint: String?
+    public let prompt: String
+}
---
+++ RabbleWabble/Models/QuestionGroup.swift
@@ -0,0 +1,14 @@
+//
+//  QuestionGroup.swift
+//  RabbleWabble
+//
+//  Created by David Andres Cespedes on 6/5/18.
+//  Copyright © 2018 David Andres Cespedes. All rights reserved.
+//
+
+import Foundation
+
+public struct QuestionGroup {
+    public let questions: [Question]
+    public let title: String
+}
